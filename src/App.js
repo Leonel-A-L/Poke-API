cae2e22aba2494060b5998083a56708cb4b08275
Leonel-A-L <lamelas.leonel@gmail.com>
2023-06-04 13:43:20 -0400

    added Routes and Router to app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
 import Gallery from './Components/Gallery'
+import Pokemon from './Components/Pokemon'
 
 function App() {
   const [data, setData] = useState([])
   const [apiUrl, setApiUrl] = useState('https://pokeapi.co/api/v2/pokemon')
   const [nextUrl, setNextUrl] = useState('')
   const [previousUrl, setPreviousUrl] = useState('')
-
-  // const BASE_API_URI = 'https://pokeapi.co/api/v2/pokemon'
-
+  
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch (apiUrl)
+      const response = await fetch(apiUrl)
       const data = await response.json()
 
-      
       setData(data.results)
       setNextUrl(data.next)
       setPreviousUrl(data.previous)
@@ -24,7 +24,7 @@ function App() {
   }, [apiUrl])
 
   const fetchPreviousPage = () => {
-    if(!previousUrl) return
+    if (!previousUrl) return
     setApiUrl(previousUrl)
   }
 
@@ -35,12 +35,20 @@ function App() {
 
   return (
     <div>
-      <h1>Pokemon!</h1>
-      <div style={{'display': 'inline-block'}} >
-        <button onClick={fetchPreviousPage}>Previous</button>
-        <button onClick={fetchNextPage}>Next</button>
-      </div>
-      <Gallery data={data} />
+      <Router>
+        <Navbar />
+        <h1>Pokemon!</h1>
+        <div style={{'display': 'inline-block'}} >
+          <button onClick={fetchPreviousPage}>Previous</button>
+          <button onClick={fetchNextPage}>Next</button>
+        </div>
+        <div>
+          <Routes>
+            <Route path='/' element={<Gallery data={data} />} />
+            <Route path='/pokemon' element={<Pokemon/>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }

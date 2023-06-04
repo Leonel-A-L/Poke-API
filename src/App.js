818254e4180c27175f6cfd7ccd6a7c8bcb837bdf
Leonel-A-L <lamelas.leonel@gmail.com>
2023-06-04 13:23:44 -0400

    pokemon api search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import Gallery from './Components/Gallery'
 
 function App() {
+  const [data, setData] = useState([])
+  const [apiUrl, setApiUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+  const [nextUrl, setNextUrl] = useState('')
+  const [previousUrl, setPreviousUrl] = useState('')
+
+  // const BASE_API_URI = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch (apiUrl)
+      const data = await response.json()
+
+      
+      setData(data.results)
+      setNextUrl(data.next)
+      setPreviousUrl(data.previous)
+    }
+
+    fetchData()
+  }, [apiUrl])
+
+  const fetchPreviousPage = () => {
+    if(!previousUrl) return
+    setApiUrl(previousUrl)
+  }
+
+  const fetchNextPage = () => {
+    if (!nextUrl) return
+    setApiUrl(nextUrl)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pokemon!</h1>
+      <div style={{'display': 'inline-block'}} >
+        <button onClick={fetchPreviousPage}>Previous</button>
+        <button onClick={fetchNextPage}>Next</button>
+      </div>
+      <Gallery data={data} />
     </div>
   );
 }
 
 export default App;
+
+// Pokemon
+// previous / next buttons
+// gallery ->
